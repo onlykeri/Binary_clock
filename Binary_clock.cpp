606794f36a4ee8b9dff608d0703977519f337d5d
onlykeri <60497797+onlykeri@users.noreply.github.com>
2021-01-19 16:21:38 +0800

    Update Binary_clock.cpp

--- Binary_clock.cpp
+++ Binary_clock.cpp
@@ -1,48 +1,48 @@
 /*
- *	������ʱ�ӣ�
- *	��3*6�����ʾʱ�䣬��һ��6������Ϊ��ʱ�����ڶ���6������Ϊ���֡���������6������Ϊ���롱
- *	��ɫ�����ʾ��ʱʱ�䣬6��������Ϊ��ɫ��������ֱ��ʾ2^0��2^1��������2^5
- *	δ�����룺ʱ�䲻һ��Ҫ�����ֻ���ʾ������������֪��ȡʱ�䣬�ɴ˹��벢�����һ����������ɫʱ��
+ *	二进制时钟：
+ *	用3*6方格表示时间，第一行6个方格为“时”，第二行6个方格为“分”，第三行6个方格为“秒”
+ *	白色方格表示此时时间，6个方格软为白色从右至左分别表示2^0，2^1，……，2^5
+ *	未来设想：时间不一定要用数字化表示，可以依靠感知获取时间，由此构想并设计另一款软件：颜色时钟
 */
-#include <graphics.h>	//	ͼ�ο�ͷ�ļ�
+#include <graphics.h>	//	图形库头文件
 #include <math.h>
 
-#define High 300	//	���ô��ڴ�С
+#define High 300	//	设置窗口大小
 #define Width 600
 
 int arry[6] = {0};	
 int *p = &arry[6];
 
-int* fun(int, int);	//	���ڽ�ʱ����ֱ�ת��Ϊ�����ƴ���������
-void time(SYSTEMTIME);	//	ʱ������
-void time_2(int, SYSTEMTIME);	//	ʱ���֧��Ŀ�ģ�Ϊ��ʹ��ʱ���롱����ͬһ���������ٴ������࣬��Լ�ռ�
+int* fun(int, int);	//	用于将时分秒分别转换为二进制存于数组中
+void time(SYSTEMTIME);	//	时间主体
+void time_2(int, SYSTEMTIME);	//	时间分支，目的：为了使“时分秒”调用同一函数，减少代码冗余，节约空间
 int oldMinute = 0;	
 int oldHour = 0;
 
 int main()
 {
 	initgraph(Width, High);	
-	BeginBatchDraw();	//	��ʼ��ͼ
+	BeginBatchDraw();	//	开始绘图
 
 	while (1)	
 	{
 		SYSTEMTIME ti;
-		GetLocalTime(&ti);		// ��ȡ��ǰʱ��
-		time(ti);				//	ʱ������
-		oldMinute = ti.wMinute;	//	������ʱ���趨Ϊ��ʱ��
+		GetLocalTime(&ti);		// 获取当前时间
+		time(ti);				//	时间运算
+		oldMinute = ti.wMinute;	//	将现在时间设定为旧时间
 		oldHour = ti.wHour;
-		FlushBatchDraw();		//	������˸
+		FlushBatchDraw();		//	避免闪烁
 //		Sleep(1);
 //		cleardevice();
 	}
 
-	EndBatchDraw();		//	������ͼ
-	closegraph();		// �رջ�ͼ����
+	EndBatchDraw();		//	结束绘图
+	closegraph();		// 关闭绘图窗口
 	return 0;
 }
 
-//	���ڽ�ʱ����ֱ�ת��Ϊ�����ƴ���������
-int* fun(int n, int sum)	//	����ȡ��2������2�������Ϊ0�����������������ȡ��2ʱ���Ϊ1��λ�ô洢��������
+//	用于将时分秒分别转换为二进制存于数组中
+int* fun(int n, int sum)	//	不断取余2，除以2，最后结果为0则运算结束，将其中取余2时结果为1的位置存储在数组中
 {
 //	arry[sum] = 0;	
     if (n%2 == 1)
@@ -55,29 +55,29 @@ int* fun(int n, int sum)	//	����ȡ��2������2�������Ϊ0�����������������ȡ��2
     return p;
 }
 
-//	ʱ������
+//	时间主体
 void time(SYSTEMTIME ti)
 {
-	setlinecolor(BLACK);	//	������ı�Ե����ɫ��Ϊ��ɫ
+	setlinecolor(BLACK);	//	将方块的边缘线颜色置为黑色
 	//for(int i=3; i > 0; i--)
 	//	time_2(i, ti);
 	time_2(3, ti);
 
-	//	Ϊ�˲�ʱ�̸��¡�ʱ���͡��֡�����Լ���ģ����Ч��
+	//	为了不时刻更新“时”和“分”，节约消耗，提高效能
 	if (ti.wMinute != oldMinute)
 		time_2(2, ti);
 	if (ti.wHour != oldHour)
 		time_2(1, ti);
 }
 
-//	ʱ���֧��Ŀ�ģ�Ϊ��ʹ��ʱ���롱����ͬһ���������ٴ������࣬��Լ�ռ�
+//	时间分支，目的：为了使“时分秒”调用同一函数，减少代码冗余，节约空间
 void time_2(int num, SYSTEMTIME ti)
 {
 	int j = 0;
-	for(int i=0; i<6; i++)	//	��������0, Ŀ�ģ�Ϊ��ʹ֮�����fun()����ʱ�����ᵼ���ظ�ʹ�������д洢��ֵ����ɡ��������󡱣���Ӱ��֣�ʱ����Ӱ��ʱ��
+	for(int i=0; i<6; i++)	//	将数组置0, 目的：为了使之后调用fun()函数时，不会导致重复使用数组中存储的值，造成“跟随现象”（秒影响分，时；分影响时）
 		arry[i] = 0;
 
-	switch(num)	//	ѡ��ʱ��ģʽΪ��ʱ/��/��
+	switch(num)	//	选择时间模式为：时/分/秒
 	{
 	case 1: 
 		fun(ti.wHour, 0);
@@ -89,16 +89,16 @@ void time_2(int num, SYSTEMTIME ti)
 		fun(ti.wSecond, 0);
 		break;
 	}
-	for (j=6; j > 0; j--)	//	��Ļ��ʾ������ʱ�䣨�÷�����ɫ��ʾ��
+	for (j=6; j > 0; j--)	//	屏幕显示二进制时间（用方块颜色表示）
 	{
-		if(arry[6-j] == 1)	//	��������ֵΪ1����Ϊ��ɫ������Ϊ��ɫ
+		if(arry[6-j] == 1)	//	若数组中值为1，则为白色，否则为黑色
 		{
-			setfillcolor(WHITE);	// ���ɫΪ����ɫ����ʾ��ʱ��ʱ��
+			setfillcolor(WHITE);	// 填充色为：白色，表示此时有时间
 			//solidrectangle(100*(j-1), 200, 100*j,300);
 			fillrectangle(100*(j-1), (num-1)*100, 100*j, num*100);   
         }else
         {
-			setfillcolor(BLACK);    // ���ɫΪ����ɫ����ʾ��ʱ��ʱ��
+			setfillcolor(BLACK);    // 填充色为：黑色，表示此时无时间
             solidrectangle(100*(j-1), (num-1)*100, 100*j, num*100);
         }		
     }
@@ -107,8 +107,8 @@ void time_2(int num, SYSTEMTIME ti)
 
 /************************************************
 *												*
-*				�����ߣ�keri					*
-*			����޸�ʱ�䣺2020/2/21				*
-*				��Ȩ����Դ						*
+*				制作者：keri					*
+*			最后修改时间：2020/2/21				*
+*				版本：v1.0.0						*
 *												*
 ************************************************/
